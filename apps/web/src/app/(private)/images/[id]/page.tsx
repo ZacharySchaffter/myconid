c66c548fe783b13524755fe1125c980a447c23e5
Zachary Schaffter <schafftz@oregonstate.edu>
2025-07-26 14:33:44 -0700

    feat: quick scaffold for image detail ui

--- apps/web/src/app/(private)/images/[id]/page.tsx
+++ apps/web/src/app/(private)/images/[id]/page.tsx
@@ -1,6 +1,10 @@
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import dayjs from "dayjs";
+import Heading from "@/components/Heading";
 import Auth from "@/services/auth";
 import Myconid from "@/services/core";
-import { notFound } from "next/navigation";
+import { LucideArrowLeft } from "lucide-react";
 
 type PageParams = {
   id: string;
@@ -25,8 +29,38 @@ const ImageDetailPage = async ({ params }: { params: Promise<PageParams> }) => {
   }
 
   return (
-    <div>
-      <img src={`/assets/images/${id}`} />
+    <div className="max-w-lg w-full">
+      <div className="mb-3">
+        <Link
+          href="/account"
+          className="inline-flex items-center gap-1 text-sm"
+        >
+          <LucideArrowLeft width={"1rem"} /> <span>Back to dashboard</span>
+        </Link>
+      </div>
+      <Heading level="h1" className="mb-4">
+        Analysis Results
+      </Heading>
+      {/* Image */}
+      <div className="relative border border-black aspect-square">
+        <img
+          src={`/assets/images/${id}`}
+          alt={`image id: ${image.id}`}
+          className="block absolute left-0 top-0 right-0 bottom-0 w-full h-full object-cover object-center"
+        />
+      </div>
+      {/* Info */}
+      <div>
+        <div className="text-sm my-2">
+          Uploaded at: {dayjs(image.createdAt).format("YYYY-MM-DD")}
+        </div>
+        <Heading level="h1" tag="h2">
+          <span className="block text-[.55em] font-medium">Genus Name</span>
+          <span>Species Name (todo)</span>
+        </Heading>
+
+        {/* TODO: Fill in data here */}
+      </div>
     </div>
   );
 };
