7e4809baae0aa1ebb2b788e8493a69278fc2b079
Zachary Schaffter <schafftz@oregonstate.edu>
2025-08-03 10:22:01 -0700

    feat: create core-api service

---
+++ apps/core-api/src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_req, res) => {
+  res.send("core-api is running");
+});
+
+let server = app.listen(PORT, () => {
+  console.log(`server running at http://localhost:${PORT}`);
+});
+
+// Graceful shutdown handler
+const gracefulShutdown = () => {
+  console.log("beginning graceful shutdown...");
+
+  server.close((err) => {
+    if (err) {
+      console.error("error during graceful shutdown:", err);
+      process.exit(1);
+    }
+    console.log("server closed, exiting process.");
+    process.exit(0);
+  });
+
+  // force after 15s
+  setTimeout(() => {
+    console.warn("force exiting after timeout");
+    process.exit(1);
+  }, 15000);
+};
+
+process.on("SIGTERM", gracefulShutdown);
+process.on("SIGINT", gracefulShutdown);
