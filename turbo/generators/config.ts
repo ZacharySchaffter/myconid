f7ec3103ea7df0b36dd1f0ac691b22e5f3a7ae06
Zachary Schaffter <schafftz@oregonstate.edu>
2025-08-03 10:11:11 -0700

    feat: add ts-express generator

---
+++ turbo/generators/config.ts
@@ -0,0 +1,37 @@
+import { PlopTypes } from "@turbo/gen";
+
+export default function (plop: PlopTypes.NodePlopAPI) {
+  plop.setGenerator("ts-express", {
+    description: "Create a new typescript-express service",
+    prompts: [
+      { type: "input", name: "SERVICE_NAME", message: "New service name" },
+      {
+        type: "input",
+        name: "DEFAULT_PORT",
+        message: "Service's dev port",
+        default: "3000", // optional default value
+        validate: (value: string) => {
+          const port = Number(value);
+          if (isNaN(port) || port < 3000 || port > 3999) {
+            return "please enter a port number between 3000 and 3999";
+          }
+          return true;
+        },
+      },
+    ],
+    actions: [
+      {
+        type: "addMany",
+        destination: "apps/{{SERVICE_NAME}}/",
+        templateFiles: "templates/ts-express/**",
+        base: "templates/ts-express",
+        globOptions: { dot: true },
+      },
+      {
+        type: "add",
+        path: "apps/{{SERVICE_NAME}}/.env",
+        template: "PORT={{DEFAULT_PORT}}\n",
+      },
+    ],
+  });
+}
