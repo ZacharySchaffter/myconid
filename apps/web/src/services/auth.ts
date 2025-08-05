17fef8053cbfc9989ae1efd016180cba6581d863
Zachary Schaffter <schafftz@oregonstate.edu>
2025-08-04 20:32:15 -0700

    feat: initial refactoring to get auth service wired up

--- apps/web/src/services/auth.ts
+++ apps/web/src/services/auth.ts
@@ -1,8 +1,6 @@
 "use server-only";
 
 import { getSessionToken } from "@/lib/session.server";
-import { cookies } from "next/headers";
-import { NextRequest } from "next/server";
 
 interface TokenResponse {
   user_id: string;
@@ -12,32 +10,59 @@ interface TokenResponse {
 }
 
 class Auth {
-  constructor() {}
+  baseUrl = process.env.NEXT_PUBLIC_AUTH_SERVICE_BASE_URL;
+  constructor() {
+    if (!this.baseUrl) {
+      console.error("AUTH_SERVICE_BASE_URL unset and required!");
+    }
+  }
+
+  async _fetch<T>(path: string, options?: RequestInit): Promise<T> {
+    return fetch(this.baseUrl + path, options).then(async (res) => {
+      let data;
+      try {
+        data = await res.clone().json();
+      } catch {
+        data = await res.text();
+      }
+
+      if (!res.ok) {
+        throw new Error(data || `${res.status} - ${res.statusText}`);
+      }
+
+      return data;
+    });
+  }
 
   async login(username: string, password: string): Promise<TokenResponse> {
-    throw Error("not implemented");
+    return this._fetch("/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+    });
   }
 
   async register(username: string, password: string): Promise<TokenResponse> {
-    throw Error("not implemented");
+    return this._fetch("/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+    });
   }
 
-  async verifySession(): Promise<
-    { userId: string; isValid: true } | { userId?: null; isValid: false }
-  > {
-    // TODO: verify token via auth service
-    const token = await getSessionToken();
-
+  async verifySession(
+    token?: string
+  ): Promise<{ userId: string; isValid: true }> {
     if (!token) {
-      return {
-        isValid: false,
-      };
+      token = (await getSessionToken()) || "";
     }
 
-    return {
-      userId: "fake-id-123",
-      isValid: true,
-    };
+    if (!token) {
+      return Promise.reject("no session found");
+    }
+
+    return this._fetch("/validate", {
+      method: "POST",
+      body: JSON.stringify({ token: token }),
+    });
   }
 }
 
